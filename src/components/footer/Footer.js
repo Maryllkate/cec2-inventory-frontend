e16c60a774bb0a497736df9ce8c9f3b072446d08
Maryllkate <maryllkate20@gmail.com>
2024-01-25 08:34:17 +0800

    initial backup

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="--flex-center --py2">
+      <p>In Fulfillment of CEC2 &copy; 2024</p>
+    </div>
+  );
+};
+
+export default Footer;
